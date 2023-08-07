dc63ae8e6d3022b143d60633f2825b23652f7e4b
Phillip Passmore <phillipspassmore@gmail.com>
2023-08-07 23:18:21 +0100

    Successful pulling of data and logging added
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- tideInfo.py
+++ tideInfo.py
@@ -1,15 +1,25 @@
 import apiInfo
-import urllib.request, json, datetime, time
+import urllib.request, json
+import logging
 
 class tidalEvents:
     def __init__(self) -> None:
+        
         pass
     
     def get_data():
+        logging.info('Requesting json file from Admiralty API')
         apiKey = apiInfo.apiKey
-        with urllib.request.urlopen(f"https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0005/TidalEvents?duration=2&key={apiKey}") as url:
-            data = json.load(url)
-            return data
-            
-data = tidalEvents.get_data()
-print(data)
+        try:
+            with urllib.request.urlopen(f"https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0005/TidalEvents?duration=2&key={apiKey}") as url:
+                data = json.load(url)
+                return data
+        except:
+            logging.error('Failed to get data from Admiralty API')
+logging.basicConfig(filename='TideInfo.log', 
+                    filemode='a', 
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+tidalEvents()
