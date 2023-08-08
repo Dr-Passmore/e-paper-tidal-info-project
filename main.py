5b01c3dcb8436b64a2921dcaef4d2cb024b4a1c8
Phillip Passmore <phillipspassmore@gmail.com>
2023-08-08 20:58:54 +0100

    data processing completed
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import tideInfo
 import logging
+import datetime
+import time
 
 class TideInformationDisplay:
     def __init__(self):
         logging.info('Initialising Data Retrieval and e-ink update')
         data = tideInfo.tidalEvents.get_data()
-        print(data)
-        pass
+        TideInformationDisplay.data_processing(self, data)
 
+    def data_processing(self, data):
+        for x in data:
+                event = (x['EventType'])
+                height = (x['Height'])
+                eventTime = (x['DateTime'])
+                eventTime = eventTime[:19]
+                
+                when = datetime.datetime.strptime(eventTime,"%Y-%m-%dT%H:%M:%S")
+                timestamp = datetime.datetime.timestamp(when)
+                now = time.time()
+                eventTime = eventTime[11:16]
+                if timestamp > now:
+                    break
+                else: 
+                    pastevent = event
+                    pastheight = height
+                    previousEventTime = eventTime
+                    prioreventTime = timestamp
+            
+        timeRemaining = (timestamp - now) / 60
+        timeSince = (now - prioreventTime) / 60
+        total = timeRemaining + timeSince
+        percentage = 100 / total
+        progress = percentage * timeSince
+        
+        if event == "LowWater":
+            progress = 100 - progress
+            
+        print(f"The next tide event is {event} at a Height of {height} meters on {eventTime}")
 
 logging.basicConfig(filename='TideInfo.log', 
                     filemode='a', 
