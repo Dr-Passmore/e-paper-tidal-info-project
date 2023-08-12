fe80d547d20e7fbe752735724083ba43dc4f6284
Phillip Passmore <phillipspassmore@gmail.com>
2023-08-12 15:45:08 +0100

    Display resources added and testing started
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- einkDisplayUpdate.py
+++ einkDisplayUpdate.py
@@ -2,28 +2,49 @@ import logging
 from PIL import Image,ImageDraw,ImageFont
 from waveshare_epd import epd2in13bc
 import time
+import sys
+import os
 
 
 class einkUpdate:
     def __init__(self) -> None:
+        
+        
         pass
-    
     def error_display(e):
         print(f"error: {e}")
         epd = epd2in13bc.EPD()
-    def loading_message():
+        HBlackimage = Image.new('1', (epd.height, epd.width), 255)
+        picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+        robotoblack32 = ImageFont.truetype(os.path.join(picdir, 'Roboto-Black.ttf'), 32)
+        drawblack = ImageDraw.Draw(HBlackimage)
+        
+        drawblack.text((2, 0), f'hello world {e}', font = robotoblack32, fill = 0)
+        epd.sleep()
+    
+    def refresh_display(epd):
+        epd.init()
+        epd.Clear()
+        time.sleep(1)
+    
+    def loading_message(epd):
         print("loading")
         
     def display_tide_info(event, height, eventTime, pastevent, pastheight, previousEventTime, progress):
         logging.info("init and Clear")
         epd = epd2in13bc.EPD()
-        epd.init()
-        epd.Clear()
-        time.sleep(1)
+        einkUpdate.refresh_display(epd)
+        
+        HBlackimage = Image.new('1', (epd.height, epd.width), 255)
+        picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+        robotoblack32 = ImageFont.truetype(os.path.join(picdir, 'Roboto-Black.ttf'), 32)
+        drawblack = ImageDraw.Draw(HBlackimage)
+        
+        drawblack.text((2, 0), 'hello world', font = robotoblack32, fill = 0)
         
         
         print("Tide info")
-        einkUpdate.loading_message()
+        einkUpdate.loading_message(epd)
         print(f"{event}: {eventTime} with a height of {height}M")
         
         
