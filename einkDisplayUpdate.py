3da128735447633c38e5230afcbefea4a9f64674
Phillip Passmore <phillipspassmore@gmail.com>
2023-08-14 17:09:25 +0100

    adding red box to top
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- einkDisplayUpdate.py
+++ einkDisplayUpdate.py
@@ -31,35 +31,43 @@ class einkUpdate:
         time.sleep(1)
     
     def loading_message(epd, robotoblack12, robotoblack18, robotoblack32):
+        
+        #212(W) x 104(H) pixel
         logging.info("loading message - Records")
         LoadingBlackimage = Image.new('1', (epd.height, epd.width), 255)
+        Other = Image.new('1', (epd.width, epd.height), 255)
         drawLoadBlack = ImageDraw.Draw(LoadingBlackimage)
+        draw_other = ImageDraw.Draw(Other)
         config = configparser.ConfigParser()
         config.read('config.ini')
         startDate = config.get('Records', 'start date')
         highHeight = config.get('Records', 'highest tide height')
         highestTideDate = config.get('Records', 'highest tide date')
-        drawLoadBlack.text((2, 0), f'Records:', font = robotoblack32, fill = 0)
+        draw_other.rectangle((0, 25, 0, 25), fill = 0)  
+        drawLoadBlack.text((2, 0), f'Records:', font = robotoblack32, fill = 1)
         drawLoadBlack.text((2, 30), f'High Tide height: {float(highHeight):.2f}', font = robotoblack18, fill = 0)
         drawLoadBlack.text((2, 50), f'Time: {highestTideDate}', font = robotoblack18, fill = 0)
         drawLoadBlack.text((2, 70), f'Since {startDate}', font = robotoblack12, fill = 0)
         epd.Clear()
-        epd.display(epd.getbuffer(LoadingBlackimage), epd.getbuffer(LoadingBlackimage))
+        epd.display(epd.getbuffer(LoadingBlackimage), epd.getbuffer(draw_other))
         
-        time.sleep(30)
+        time.sleep(180)
         
         einkUpdate.refresh_display(epd)
         
     def display_tide_info(event, height, eventTime, pastevent, pastheight, previousEventTime, progress):
+        #212(H) x 104(V) pixel
         logging.info("init and Clear")
         epd = epd2in13bc.EPD()
         einkUpdate.refresh_display(epd)
         
         HBlackimage = Image.new('1', (epd.height, epd.width), 255)
+        Other = Image.new('1', (epd.width, epd.height), 255)
         robotoblack32 = ImageFont.truetype('pic/Roboto-Black.ttf', 32)
         robotoblack18 = ImageFont.truetype('pic/Roboto-Black.ttf', 18)
         robotoblack12 = ImageFont.truetype('pic/Roboto-Black.ttf', 12)
         drawblack = ImageDraw.Draw(HBlackimage)
+        draw_other = ImageDraw.Draw(Other)
         einkUpdate.loading_message(epd, robotoblack12, robotoblack18, robotoblack32)
         
         #drawblack.text((2, 0), 'hello world', font = robotoblack32, fill = 0)
