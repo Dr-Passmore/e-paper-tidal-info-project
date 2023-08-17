d918013933af26e851957285ea3ff29911026187
Phillip Passmore <phillipspassmore@gmail.com>
2023-08-17 23:10:28 +0100

    draw progress bar updated
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- einkDisplayUpdate.py
+++ einkDisplayUpdate.py
@@ -116,11 +116,11 @@ class einkUpdate:
         drawblack.text((2, 20), f'height: {height:.2f}', font = robotoblack18, fill = 0)
         drawblack.text((2, 40), f'Time: {eventTime}', font = robotoblack18, fill = 0)
         
-        draw_other.rectangle((20, 60, progressDraw, 10), fill=0)
+        draw_other.rectangle((20, 60, 40, progressDraw), fill=0)
         print("Tide info")
         
         print(f"{event}: {eventTime} with a height of {height}M")
-        print(f"{progressDraw}%")
+        print(f"{progressDraw}")
         
         
         epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
