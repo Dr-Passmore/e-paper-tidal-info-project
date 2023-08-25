214044c6f53d06926e3fea9b7aabca76b25366d1
Phillip Passmore <phillipspassmore@gmail.com>
2023-08-25 19:43:12 +0100

    error is erroring - fix1
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- tideInfo.py
+++ tideInfo.py
@@ -22,17 +22,21 @@ class tidalEvents:
         config = configparser.ConfigParser()
         config.read('config.ini')
         apiKey = config.get('API Key', 'Key')
-        
         try:
-            with urllib.request.urlopen(f"https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0005/TidalEvents?duration=2&key={apiKey}") as url:
-                data = json.load(url)
-                logging.info('Request completed')
-                return data
+            try:
+                with urllib.request.urlopen(f"https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0005/TidalEvents?duration=2&key={apiKey}") as url:
+                    data = json.load(url)
+                    logging.info('Request completed')
+                    return data
+            except Exception as e:
+                logging.error('Failed to get data from Admiralty API')
+                logging.error(f'{e}')
+                einkDisplayUpdate.einkUpdate.error_display(e)
         except Exception as e:
-            logging.error('Failed to get data from Admiralty API')
-            logging.error(f'{e}')
-            einkDisplayUpdate.einkUpdate.error_display(e)
-
+                logging.error('Failed to get data from Admiralty API')
+                logging.error(f'{e}')
+                einkDisplayUpdate.einkUpdate.error_display(e)
+                
 logging.basicConfig(filename='TideInfo.log', 
                     filemode='a', 
                     level=logging.INFO,
