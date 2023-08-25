933cb2178b6f58eb27452a7927295e6a2464460f
Phillip Passmore <phillipspassmore@gmail.com>
2023-08-25 19:49:27 +0100

    error layout updated
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- einkDisplayUpdate.py
+++ einkDisplayUpdate.py
@@ -25,8 +25,8 @@ class einkUpdate:
         robotoblack14 = ImageFont.truetype('pic/Roboto-Black.ttf', 14)
         drawblack = ImageDraw.Draw(HBlackimage)
         draw_other = ImageDraw.Draw(HRedimage)
-        drawblack.text((60, 0), f'{e}', font = robotoblack32, fill = 0)
-        draw_other.text((10, 65), "ERROR!", font=robotoblack14, fill=0)
+        drawblack.text((10, 65), f'{e}', font = robotoblack14, fill = 0)
+        draw_other.text((60, 10), "ERROR!", font=robotoblack32, fill=0)
         
         epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
         epd.sleep()
